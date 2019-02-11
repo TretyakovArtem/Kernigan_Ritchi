b99b30cd45a3315eb3f19443ae69398b89f32b5d
a_tretyakov <artem.tretyakov.91@inbox.ru>
2019-02-11 18:48:07 +0300

    1.9

---
+++ chapter_1/1.5/1.5.3/change_tabs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+
+    while((c = getchar()) != EOF) {
+        if (c == '\t') {
+            printf("\\t");
+
+        }
+
+        if (c == '\b') {
+            printf("\\b");
+        }
+        putchar(c);
+    }
+}
